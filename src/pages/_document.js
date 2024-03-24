255807df0746cac7e978063fd5bb02bccf955bb5
Yash Bhardwaj <theyashbhardwaj@gmail.com>
2024-03-25 00:24:39 +0530

    added og+opengraph

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta property="twitter:image" content="/opengraph.png"/>
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:title" content="Pondi Life"></meta>
+      <meta property="twitter:description" content="the exhaustive list of food,stay & things to do in pondi"></meta>
+      <meta property="description" content="the exhaustive list of food,stay & things to do in pondi" />
+      <meta property="og:image" content="/opengraph.png"></meta>
+      <meta property="og:title" content="Pondy Life"></meta>
+      <meta property="og:description" content="the exhaustive list of food,stay & things to do in pondi"/>
+      <meta property="og:url" content="🏖️pondy.life"></meta> </Head>
       <body>
         <Main />
         <NextScript />
