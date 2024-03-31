2faa36ecce6fa5e4b15c185199ee3c5bff0d87bf
Yash Bhardwaj <theyashbhardwaj@gmail.com>
2024-04-01 01:49:58 +0530

    database connected

--- src/pages/components/ContentGrid.js
+++ src/pages/components/ContentGrid.js
@@ -1,10 +1,9 @@
 // components/ContentGrid.js
 
-export default function ContentGrid({ title, emoji, items = [] }) { // Set a default value for items
+export default function ContentGrid({ title, emoji, items = [] }) {
   // Check if items is an array and has length before rendering the list
   if (!Array.isArray(items) || items.length === 0) {
-    // You can return a loading indicator or null if there are no items
-    return <div>Loading...</div>; // Or return null;
+    return <div>Loading...</div>; // Or return null if no loading indicator is needed
   }
 
   return (
@@ -15,19 +14,23 @@ export default function ContentGrid({ title, emoji, items = [] }) { // Set a def
         {/* Absolute-positioned full-width underline for the heading */}
         <div className="absolute bottom-0 left-0 w-full border-b border-gray-300"></div>
       </h2>
-      <ul className="pl-4 counter-reset pb-4">
+      <ul className="pl-4 counter-reset pb-2">
         {items.map((item, index) => (
           <li key={index} className="mb-1 counter-increment group p-0.5">
-            <a href={item.href} className="flex items-center no-underline text-sm" target="_blank" rel="noopener noreferrer">
+            <a 
+              href={item.url} 
+              className="flex items-center no-underline text-sm" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
               {/* Counter */}
               <span className="counter text-slate-300 mr-2"></span>
               {/* Text with conditional underline on hover */}
-              <span className="text-slate-500 underline underline-offset-4">{item.text}</span>
+              <span className="text-slate-500 underline underline-offset-4">{item.name}</span>
               {/* Separate span for the arrow to prevent it from being underlined */}
               <span className="ml-2 text-slate-500 transform rotate-[-45deg] inline-block group-hover:no-underline">→</span>
             </a>
             {/* Full-width underline for each list item */}
-            <div className="absolute bottom-0 left-0 w-full"></div>
           </li>
         ))}
       </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,57 @@
-import Header from '../pages/components/Header'; 
-import Hero from '../pages/components/Hero';     
-import ContentGrid from '../pages/components/ContentGrid'; 
-import Footer from '../pages/components/Footer'; 
-
+// pages/index.js
+import { useEffect, useState } from 'react';
+import { supabase } from '../../utils/supabaseClient';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ContentGrid from './components/ContentGrid';
+import Footer from './components/Footer';
 
 export default function Home() {
-  
-  const beachItems = [
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Promenade Beach' },
-    { href: 'https://maps.app.goo.gl/wrNHiVZnvLXdGtUW8', text: 'Serenity Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Mahe Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Auroville Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Paradise Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Reppo Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Veerampattinam Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Karaikal Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Mahabalipuram Beach' },
-    { href: 'https://maps.app.goo.gl/Qdvraok1mphzcGRu6', text: 'Quiet Beach' },
-    
-  ];
+  const [combinedData, setCombinedData] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const { data: categories, error: categoriesError } = await supabase
+          .from('categories')
+          .select('id, category_name, emoji');
+        if (categoriesError) throw categoriesError;
+
+        const { data: items, error: itemsError } = await supabase
+          .from('items')
+          .select('name, url, relation');
+        if (itemsError) throw itemsError;
+
+        // Combine categories and items
+        const combinedData = categories.map(category => ({
+          ...category,
+          items: items.filter(item => item.relation === category.id),
+        }));
+
+        setCombinedData(combinedData);
+      } catch (error) {
+        console.error('Error loading data:', error);
+      }
+    }
+
+    loadData();
+  }, []);
+  console.log(combinedData);
+
 
   return (
     <div>
       <Header />
       <Hero />
       <main className="bg-white max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-8">
-        <ContentGrid title="Beaches" emoji="⛱️" items={beachItems} />
-        <ContentGrid title="AirBnB" emoji="🛖" items={beachItems} />
-        <ContentGrid title="Bike Rentals" emoji="🛵" items={beachItems} />
-        <ContentGrid title="Surf Schools" emoji="🏄🏻‍♂️" items={beachItems} />
-        <ContentGrid title="Breakfast" emoji="🍞" items={beachItems} />
-        <ContentGrid title="Lunch" emoji="🍽️" items={beachItems} />
-        <ContentGrid title="Coffee" emoji="☕" items={beachItems} />
-        <ContentGrid title="Dinner" emoji="🍲" items={beachItems} />
-        {}
+        {combinedData.map((category) => (
+          <ContentGrid
+            key={category.id}
+            title={category.category_name}
+            emoji={category.emoji}
+            items={category.items}
+          />
+        ))}
       </main>
       <Footer />
     </div>
