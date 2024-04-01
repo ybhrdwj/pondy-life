23cddcd27db44e1b845a4c983627a325aa4fae74
Yash Bhardwaj <theyashbhardwaj@gmail.com>
2024-04-01 20:18:50 +0530

    mobile fixes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta property="twitter:image" content="/opengraph.png"/>
+      <meta property="twitter:image" content="https://pondy.life/opengraph.png"/>
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:title" content="Pondi Life"></meta>
       <meta property="twitter:description" content="the exhaustive list of food,stay & things to do in pondi"></meta>
       <meta property="description" content="the exhaustive list of food,stay & things to do in pondi" />
-      <meta property="og:image" content="/opengraph.png"></meta>
+      <meta property="og:image" content="https://pondy.life/opengraph.png"></meta>
       <meta property="og:title" content="Pondy Life"></meta>
       <meta property="og:description" content="the exhaustive list of food,stay & things to do in pondi"/>
-      <meta property="og:url" content="🏖️pondy.life"></meta> </Head>
+      <meta property="og:url" content="pondy.life"></meta> </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,14 +1,15 @@
 export default function Footer() {
     return (
-        <footer className="max-w-6xl mx-auto bg-white py-4 px-6 flex flex-col sm:flex-row justify-between items-center border-t-slate-300">
-            
-            <div className=" text-slate-600 sm:text-left">made shortly after our pondy trip in Mar 2024</div>
-        <div className="text-gray-600 sm:text-right">by <a href="https://x.com/neilshroff"target="_blank">@neilshroff</a>
-        <a href="https://x.com/paarugsethi"target="_blank">,@paarugsethi</a>
-        <a href="https://x.com/pratikdholani"target="_blank">,@pratikdholani</a>
-        <a href="https://x.com/ybhrdwj"target="_blank">,@ybhrdwj</a>
-        <a href="https://x.com/abhwshek"target="_blank">@abhwshek</a></div>
-            
+        <footer className="max-w-6xl mx-auto bg-white py-4 px-6 flex overflow-x-auto justify-between items-center ">
+            <div className="text-slate-600 whitespace-nowrap">made by the boys, shortly after our pondy trip</div>
+            <div className="flex space-x-2 text-slate-500">
+                <a href="https://x.com/neilshroff" target="_blank" rel="noopener noreferrer">@neilshroff</a>,
+                <a href="https://x.com/paarugsethi" target="_blank" rel="noopener noreferrer">@paarugsethi</a>,
+                <a href="https://x.com/pratikdholani" target="_blank" rel="noopener noreferrer">@pratikdholani</a>,
+                <a href="https://x.com/ybhrdwj" target="_blank" rel="noopener noreferrer">@ybhrdwj</a>,
+                {/* Make this last link optional based on screen size */}
+                <a href="https://x.com/abhwshek" target="_blank" rel="noopener noreferrer" className="sm:block hidden">@abhwshek</a>
+            </div>
         </footer>
     )
 }
